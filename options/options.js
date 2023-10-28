2edf2d2abd86e3ca11f58fe604ba64256576c537
Crowdbotics <team@crowdbotics.com>
2023-10-28 19:12:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trust-business-bank-44195",
+  url: "https://trust-business-bank-44195.botics.co",
+  api: "https://trust-business-bank-44195.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
